3785901cc8d1322e8d655003a3ab8033354a8103
boborazokov <boborazokovfozil01@gmail.com>
2024-06-08 11:46:19 +0500

    fix bags in functions

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let screnHeight = document.querySelector("body").scrollHeight / 3;
 function origin() {
   document.documentElement.scrollTop = screnHeight;
 }
+
+function allowDrop(evevt) {
+  evevt.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
